3ce2ba522b3a37286fa8430b909b02af0a80e820
3DGamerexe <acmadiso@ucsc.edu>
2024-02-19 23:25:10 -0600

    Completed IH method

--- NodeHandling/NodeHandling.cpp
+++ NodeHandling/NodeHandling.cpp
@@ -5,25 +5,79 @@
 #include <string>
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node* next;
+class IntSLLNode {
 
-    Node() {
-        data = 0;
-        next = NULL;
+public:
+    class SinglyLinkedList;  
+
+    IntSLLNode() {
+        next = 0;
     }
+    IntSLLNode(string i, IntSLLNode* in = nullptr) {
+        info = i; 
+        next = in;
+    }
+    string info;
+    IntSLLNode* next;  
+    
 };
 
 
-class SinglyLinkedList {
-    Node* head;
+class IntSLLNode::SinglyLinkedList {
+public: 
+    IntSLLNode* head;
+    IntSLLNode* tail;
 
-public:
+    SinglyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+    
+    void IH(string el) {
 
-    void IH(string v) {
-        
+        if (el.length() > 1) {
+            cout << "The value you entered should be a single character or digit." << endl;
+        }
+        else if (el.length() == 1) {
+            head = new IntSLLNode(el, head);
+            if (tail == 0) {
+                tail = head;
+            }
+        }
+
+        /*head = new IntSLLNode(el, head);
+        if (tail == 0) {
+            tail = head;
+        }*/
+
+        //if (el.length() > 1) {
+        //    cout << "The value you enter in the LinkedList should be a single character or digit." << endl;
+        //}
+
+        //else if (el.length() == 1) {
+        //    //head = new IntSLLNode(el, head);
+        //    IntSLLNode* newNode = new IntSLLNode(el, head);
+        //    head = newNode; 
+
+        //    if (tail == nullptr) {
+        //        tail = head;
+        //    }
+        //    PS(head);
+        //}       
+    }
+
+    void PS() {
+        IntSLLNode* current = head; 
+
+        while (current != 0) {
+            cout << current->info << " ";
+            current = current->next;
+        }
+        cout << endl;
+
+        /*if (head != NULL) {
+            cout << head -> info << endl;
+        }*/
     }
 };
 
@@ -31,49 +85,45 @@ public:
 
 void Menu() {
     std::cout << "\n";
-    std::cout << "\t\tM E N U\n";
-    std::cout << "SLL: IH(0), IT(1), DH(2), DT(3), SD(4), PS(5)\n";
-    std::cout << "DLL: IH(6), IT(7), DH(8), DT(9), SD(10), PD(11)\n\n";
-    std::cout << "Exit Program(12)\n";
+    std::cout << "\t\tM E N U" << endl;
+    std::cout << "SLL: IH(0), IT(1), DH(2), DT(3), SD(4), PS(5)" << endl;
+    std::cout << "DLL: IH(6), IT(7), DH(8), DT(9), SD(10), PD(11)" << endl << endl;
+    std::cout << "Exit Program(12)" << endl;
 }
 
 int main()
-{
-    Menu();
-
+{    
     int menuValue;
-    std::string value;
-    std::cin >> menuValue;
-    std::cin >> value;
+    std::string value;  
+    IntSLLNode::SinglyLinkedList single;
 
+     do {
+        Menu();
+        std::cin >> menuValue;
 
-
-    while (menuValue != 12) {
         if (menuValue < 0 || menuValue > 12 || cin.fail()) {
             cout << "Invalid. Enter a valid number provided in the Menu.";
         }
+
         else {
-            cout << "Valid. Thank you.";
 
             if (menuValue <= 5) {
-                SinglyLinkedList single;
-
+                          
                 switch (menuValue) {
                 case 0:
+                    std::cin >> value;
                     single.IH(value);
+                    break;
+                case 5:
+                    single.PS();
                 }
-                    
-            }
-            /*else {
-                DoublyLinkedList doubly(menuValue);
-            }*/
-            
+            }    
         }
-        Menu();
-        std::cin >> menuValue;
-    }
-    if (menuValue == 12) {
-        cout << "Thank you! Have a nice day!";
-    }
+        
+        if (menuValue == 12) {
+            cout << "Thank you! Have a nice day!";
+        }
+
+     } while (menuValue != 12);
     
 }
